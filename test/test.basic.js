8363d90b5e05a6d6a8476aa5863c96164e17af18
simw <simw@users.noreply.github.com>
2014-04-29 15:21:33 -0700

    Time module - convert to UTC on find

--- test/test.basic.js
+++ test/test.basic.js
@@ -5,9 +5,9 @@ var expect = require('chai').expect,
     mware = require('../index');
 
 var config = {
-    db: 'mongodb://localhost:27017/nbdb',
-    collection: 'articles'
-}; 
+    db: 'mongodb://localhost:27017/mongo-in-the-middle-tests',
+    collection: 'testing'
+};
 
 suite('basic', function() {
     test('Basic object creation', function(done) {
@@ -22,5 +22,43 @@ suite('basic', function() {
         app.init(function() {
             done();
         });
+    });  
+  
+    test('Insert, update, find and remove with middleware', function(done) {
+        var app = new Mitm(config);
+        for (var mw in mware) {
+            if (typeof(mware[mw]) === "Function") {
+                app.use(mware[mw]());
+            } else {
+                app.use(mware[mw]);
+            }
+        }
+        app.init(function() {
+            expect(app.testedInit).to.exist;
+
+            var entry = {hello: "World"};
+            app.insert(entry, function(err, doc) {
+                expect(!err).to.be.ok;
+                expect(doc[0]._id).to.exist;
+                expect(app.testedInsertBefore).to.exist;
+                expect(app.testedInsertAfter).to.exist;
+
+                app.findById(doc[0]._id, function(err, items) {
+                    expect(items).to.have.length(1);
+                    expect(app.testedFindBefore).to.exist;
+                    expect(app.testedFindAfter).to.exist;
+
+                    app.remove(doc[0]._id, function(err, res) {
+                        expect(!err).to.be.ok;
+                        expect(res).to.equal(1);
+                        expect(app.testedRemoveBefore).to.exist;
+                        expect(app.testedRemoveAfter).to.exist;
+                        done();
+                    });
+                });
+            });
+        });
     });
+
+
 });
